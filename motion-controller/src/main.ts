ae0351278008535b6b6ab092fc9efdfdfb541fdd
Noeri Huisman <mrxz@users.noreply.github.com>
2023-08-01 18:31:30 +0200

    motion-controller: WIP handling of grip and targetRay spaces

--- motion-controller/src/main.ts
+++ motion-controller/src/main.ts
@@ -1,3 +1,4 @@
 import 'aframe';
 export * from './motion-controller-model.component';
+export * from './motion-controller-space.component';
 export * from './motion-controller.system';
---
+++ motion-controller/src/motion-controller-space.component.ts
@@ -0,0 +1,44 @@
+import * as AFRAME from 'aframe';
+import { strict } from 'aframe-typescript';
+
+export const MotionControllerSpaceComponent = AFRAME.registerComponent('motion-controller-space', strict<{
+    motionControllerSystem: AFRAME.Systems['motion-controller'],
+    inputSource: XRInputSource|undefined,
+}>().component({
+    schema: {
+        hand: { type: 'string', oneOf: ['left', 'right'], default: 'left' },
+        space: { type: 'string', oneOf: ['gripSpace', 'targetRaySpace'], default: 'targetRaySpace' },
+    },
+    init: function() {
+        this.motionControllerSystem = this.el.sceneEl.systems['motion-controller'];
+        this.el.sceneEl.addEventListener('motion-controller-change', _event => {
+            const inputSourceRecord = this.motionControllerSystem[this.data.hand];
+            this.inputSource = inputSourceRecord?.xrInputSource;
+        });
+    },
+    tick: function() {
+        const xrFrame = this.el.sceneEl.frame;
+        const xrReferenceSpace = this.el.sceneEl.renderer.xr.getReferenceSpace();
+        if(!this.inputSource || !xrFrame || !xrReferenceSpace) {
+            return;
+        }
+
+        const xrSpace = this.inputSource[this.data.space];
+        if(!xrSpace) {
+            return;
+        }
+
+        // FIXME: Consider getting the pose in the system and caching for subsequent retrievals
+        const xrPose = xrFrame.getPose(xrSpace, xrReferenceSpace);
+        if(xrPose) {
+            this.el.object3D.matrix.fromArray(xrPose.transform.matrix);
+            this.el.object3D.matrix.decompose(this.el.object3D.position, this.el.object3D.quaternion, this.el.object3D.scale);
+        }
+    }
+}));
+
+declare module "aframe" {
+    export interface Components {
+        "motion-controller-space": InstanceType<typeof MotionControllerSpaceComponent>
+    }
+}
