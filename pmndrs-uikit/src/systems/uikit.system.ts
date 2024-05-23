d3e5d218fe6f64200c7072523b147c24b2a17dc4
Noeri Huisman <mrxz@users.noreply.github.com>
2024-05-23 12:20:47 +0200

    pmndrs-uikit: Set required transparent sort method

--- pmndrs-uikit/src/systems/uikit.system.ts
+++ pmndrs-uikit/src/systems/uikit.system.ts
@@ -1,8 +1,12 @@
+import { reversePainterSortStable } from "@pmndrs/uikit";
 import * as AFRAME from "aframe";
 
 const UikitSystem = AFRAME.registerSystem('uikit', {
     schema: {
         useSystemKeyboard: { type: 'boolean', default: true }
+    },
+    init: function() {
+        this.sceneEl.renderer.setTransparentSort(reversePainterSortStable)
     },
     shouldUseSystemKeyboard() {
         // @ts-ignore isSystemKeyboardSupported not part of @types/webxr
